0e7eb8db8730aec6a1cca7ca0cbf06ddc0c742ba
dengelhardt <dengelhardt@evoila.de>
2022-01-17 15:55:15 +0100

    command ping

--- scripts/script_show_output.sh
+++ scripts/script_show_output.sh
@@ -4,4 +4,4 @@ ls -la outputs
 cat outputs/unname.txt
 
 nslookup dengelhardt-cluster.api.tkgi-lab.hob.local
-ping -5 dengelhardt-cluster.api.tkgi-lab.hob.local
+ping -c 5 dengelhardt-cluster.api.tkgi-lab.hob.local
