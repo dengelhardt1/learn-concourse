10b9413d4f4bc6b60744920f9dd40500c77b372a
dengelhardt <dengelhardt@evoila.de>
2022-01-18 12:33:42 +0100

    change image

--- scripts/script_show_output.sh
+++ scripts/script_show_output.sh
@@ -2,8 +2,7 @@
 
 ls -la outputs
 cat outputs/unname.txt
-apt install curl
-nslookup dengelhardt-cluster.api.tkgi-lab.hob.local
-ping -c 5 dengelhardt-cluster.api.tkgi-lab.hob.local
-ping -c 5 10.5.65.171
+
+ping -c 2 dengelhardt-cluster.api.tkgi-lab.hob.local
+ping -c 2 10.5.65.171
 curl -I http://10.5.65.171:9000
