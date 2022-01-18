7be56188e4215fe586077cf5fe7c4e381f6c406f
dengelhardt <dengelhardt@evoila.de>
2022-01-18 12:28:35 +0100

    add ping for testing

--- scripts/script_show_output.sh
+++ scripts/script_show_output.sh
@@ -5,3 +5,5 @@ cat outputs/unname.txt
 
 nslookup dengelhardt-cluster.api.tkgi-lab.hob.local
 ping -c 5 dengelhardt-cluster.api.tkgi-lab.hob.local
+ping -c 5 10.5.65.171
+curl -I http://10.5.65.171:9000
