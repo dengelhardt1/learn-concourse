36a85634fa4e41518b386e72530597bf1139c1ef
dengelhardt <dengelhardt@evoila.de>
2022-01-20 14:52:42 +0100

    new pipeline feature

--- scripts/script_output.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-touch outputs/uname.txt
-echo $(uname -a) > outputs/"uname-`date '+%Y-%m-%d-%H-%M-%S'`.txt"
--- scripts/script_show_output_s3.sh
+++ scripts/script_show_output_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ls -laR resource-s3
-cat resource-s3/uname-*.txt
+cat resource-s3/*.txt
